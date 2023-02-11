22f0b98b9709e85735515e470eeb534746f1304e
Guillermo Santos <guillermo.santosba@gmail.com>
2023-02-11 17:06:40 -0600

    Added setup py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Implementación de el algoritmo para una red Bayesiana.'
+LONG_DESCRIPTION = 'Un paquete que permite crear redes bayesianas y realizar inferencia.'
+
+# Setting up
+setup(
+    name="gbnnetwork",
+    version=VERSION,
+    author="Javier",
+    author_email="sebasvalle17@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[''],
+    keywords=['python', 'bayes'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
